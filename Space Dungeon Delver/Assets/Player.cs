6b196d0bef0aea50dee8f27698076a6ff3417ed1
JamesTech <jamestech.da.wizard@gmail.com>
2020-10-22 11:58:30 -0400

    week 1 mileston push

---
+++ Space Dungeon Delver/Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float _speed = 5;
+    private Rigidbody2D playerBody;
+    private Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movement = Vector3.zero;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if (movement != Vector3.zero)
+        {
+            MovePlayer();
+        }
+        
+    }
+
+    void MovePlayer()
+    {
+        playerBody.MovePosition(
+            transform.position + movement.normalized * _speed * Time.deltaTime
+            );
+        Debug.Log(movement * _speed * Time.deltaTime);
+    }
+}
---
+++ Space Dungeon Delver/Assets/RoomMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class RoomMove : MonoBehaviour
+{
+    [SerializeField] Camera curRoomCam;
+    [SerializeField] Camera rightRoomCam;
+
+    private void Start()
+    {
+        curRoomCam.enabled = true;
+        rightRoomCam.enabled = false;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter()
+    {
+        Debug.Log("it worked");
+        curRoomCam.enabled = false;
+        rightRoomCam.enabled = true;
+    }
+}
