fafd71b4dbf807636657e03dde708fdbc52996ab
JamesTech <jamestech.da.wizard@gmail.com>
2020-12-02 21:52:06 -0600

    Final Milestone 2

---
+++ Space Dungeon Delver/Assets/PauseGame.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if(gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                Cursor.visible = true;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                Cursor.visible = false;
+                gamePaused = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    public void UnpauseGame()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+        gamePaused = false;
+        Time.timeScale = 1;
+    }
+
+    public void ReturnMainMenu()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = true;
+        gamePaused = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
